ce700f619b350c8f06282b7782995459e165512e
Susanne Höllbacher <susanne.hoellbacher@gcsc.uni-frankfurt.de>
2014-10-20 15:57:39 +0000

    new parameter 'ReferenceObjectID' added to method 'prep_elem()' of class IElemDisc;

--- buffer_fv1_impl.h
+++ buffer_fv1_impl.h
@@ -136,7 +136,7 @@ void BufferFV1<TDomain>:: fsh_elem_loop()
 template<typename TDomain>
 template<typename TElem, typename TFVGeom>
 void BufferFV1<TDomain>::
-prep_elem(const LocalVector& u, GridObject* elem, const MathVector<dim> vCornerCoords[])
+prep_elem(const LocalVector& u, GridObject* elem, const ReferenceObjectID roid, const MathVector<dim> vCornerCoords[])
 {
 	// update geometry for this element
 	static TFVGeom& geo = GeomProvider<TFVGeom>::get();
