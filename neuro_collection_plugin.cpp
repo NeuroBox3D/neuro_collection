768770f044b07f78d2a1304ab0432a2a3a95b5f7
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2017-03-03 12:39:38 +0100

    Somehow, the last commit does not seem to have worked as expected. Correction.

--- neuro_collection_plugin.cpp
+++ neuro_collection_plugin.cpp
@@ -49,8 +49,8 @@
 #include "stimulation/action_potential_train.h"
 #include "grid_generation/bouton_generator/bouton_generator.h"
 
-#include "lib_grid/refinement/projectors/neurite_projector.h"
-#include "test/test_neurite_proj.h"
+//#include "lib_grid/refinement/projectors/neurite_projector.h"
+//#include "test/test_neurite_proj.h"
 
 #include "util/measurement.h"
 #include "lib_disc/function_spaces/grid_function.h"
@@ -555,6 +555,7 @@ static void Common(Registry& reg, string grp)
                          "Generates a drosophila NMJ bouton volume grid.");
 	}
 
+/*
 	// test neurite projector
 	{
         reg.add_function("test_neurite_projector", &test_neurite_projector_with_four_section_tube, "", "", "");
@@ -563,6 +564,7 @@ static void Common(Registry& reg, string grp)
         //reg.add_function("apply_neurite_projector", &apply_neurite_projector, "", "multigrid, neurite projector", "");
         reg.add_function("test_cylinder_volume_projector", &test_cylinder_volume_projector, "", "", "");
 	}
+*/
 }
 
 }; // end Functionality
