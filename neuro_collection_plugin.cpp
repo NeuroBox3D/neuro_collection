1fe0ca415612fa3d6767f216af478677499d20b9
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2017-12-28 17:59:24 +0100

    DO NOT COMMIT YET -- taking experimental neuron projector out of build for VRL (error)

--- neuro_collection_plugin.cpp
+++ neuro_collection_plugin.cpp
@@ -755,6 +755,7 @@ static void Common(Registry& reg, string grp)
                          "Generates a drosophila NMJ bouton volume grid.");
 	}
 
+#ifndef UG_FOR_VRL
 	// test neurite projector
 	{
 		reg.add_function("test_smoothing", &test_smoothing, "", "", "");
@@ -764,6 +765,7 @@ static void Common(Registry& reg, string grp)
 		//reg.add_function("apply_neurite_projector", &apply_neurite_projector, "", "multigrid, neurite projector", "");
 		reg.add_function("test_cylinder_volume_projector", &test_cylinder_volume_projector, "", "", "");
 	}
+#endif
 }
 
 }; // end Functionality
