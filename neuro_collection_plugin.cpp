ad11a8a4568fa674edc289f0b4b98518bdb23ab6
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2018-05-24 13:42:43 +0200

    include neurite projector again

--- neuro_collection_plugin.cpp
+++ neuro_collection_plugin.cpp
@@ -60,8 +60,8 @@
 #include "stimulation/action_potential_train.h"
 #include "grid_generation/bouton_generator/bouton_generator.h"
 
-//#include "lib_grid/refinement/projectors/neurite_projector.h"
-//#include "test/test_neurite_proj.h"
+#include "lib_grid/refinement/projectors/neurite_projector.h"
+#include "test/test_neurite_proj.h"
 
 #include "util/measurement.h"
 #include "util/ca_wave_util.h"
@@ -889,7 +889,6 @@ static void Common(Registry& reg, string grp)
                          "Generates a drosophila NMJ bouton volume grid.");
 	}
 
-/*
 #ifndef UG_FOR_VRL
 	// test neurite projector
 	{
@@ -901,7 +900,6 @@ static void Common(Registry& reg, string grp)
 		reg.add_function("test_cylinder_volume_projector", &test_cylinder_volume_projector, "", "", "");
 	}
 #endif
-*/
 }
 
 }; // end Functionality
@@ -922,8 +920,8 @@ InitUGPlugin_neuro_collection(Registry* reg, string grp)
 	typedef neuro_collection::Functionality Functionality;
 
 	GlobalAttachments::declare_attachment<ANumber>("diameter");
-//	typedef Attachment<NeuriteProjector::SurfaceParams> NPSurfParam;
-//	GlobalAttachments::declare_attachment<NPSurfParam>("npSurfParams", true);
+	typedef Attachment<NeuriteProjector::SurfaceParams> NPSurfParam;
+	GlobalAttachments::declare_attachment<NPSurfParam>("npSurfParams", true);
 /*
 	// most (if not all) algebra-dependent code is only meant for non-blocked algebras
 	typedef boost::mpl::list
