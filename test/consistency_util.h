56c3538e784f67c952efee6d1835af84f6b44bda
stephan <stephan@syntaktischer-zucker.de>
2021-02-10 15:24:57 +0100

    Adding consistency utilities.
    1) Diameter variability a nd
    2) Close by branching point filter

---
+++ test/consistency_util.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2009-2021: G-CSC, Goethe University Frankfurt
+ *
+ * Author: Stephan Grein
+ * Creation date: 2021-02-10
+ *
+ * This file is part of NeuroBox, which is based on UG4.
+ *
+ * NeuroBox and UG4 are free software: You can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3
+ * (as published by the Free Software Foundation) with the following additional
+ * attribution requirements (according to LGPL/GPL v3 §7):
+ *
+ * (1) The following notice must be displayed in the appropriate legal notices
+ * of covered and combined works: "Based on UG4 (www.ug4.org/license)".
+ *
+ * (2) The following notice must be displayed at a prominent place in the
+ * terminal output of covered works: "Based on UG4 (www.ug4.org/license)".
+ *
+ * (3) The following bibliography is recommended for citation and must be
+ * preserved in all covered files:
+ * "Reiter, S., Vogel, A., Heppner, I., Rupp, M., and Wittum, G. A massively
+ *   parallel geometric multigrid solver on hierarchically distributed grids.
+ *   Computing and visualization in science 16, 4 (2013), 151-164"
+ * "Vogel, A., Reiter, S., Rupp, M., Nägel, A., and Wittum, G. UG4 -- a novel
+ *   flexible software system for simulating PDE based models on high performance
+ *   computers. Computing and visualization in science 16, 4 (2013), 165-179"
+ * "Stepniewski, M., Breit, M., Hoffer, M. and Queisser, G.
+ *   NeuroBox: computational mathematics in multiscale neuroscience.
+ *   Computing and visualization in science (2019).
+ * "Breit, M. et al. Anatomically detailed and large-scale simulations studying
+ *   synapse loss and synchrony using NeuroBox. Front. Neuroanat. 10 (2016), 8"
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ */
+
+#ifndef UG__PLUGINS__NEURO_COLLECTION__TEST__CONSISTENCY_UTIL_H
+#define UG__PLUGINS__NEURO_COLLECTION__TEST__CONSISTENCY_UTIL_H
+
+#include "lib_grid/grid/grid.h"
+#include "lib_grid/grid/geometry.h"
+#include "lib_grid/refinement/projectors/neurite_projector.h"
+#include "common/math/ugmath_types.h"
+#include "types.h"
+
+namespace ug {
+	namespace neuro_collection {
+        /// FRAGMENTS: Pair<Position, Radius>
+        typedef std::pair<std::vector<std::vector<vector3> >, std::vector<std::vector<number> > > FRAGMENTS;
+        /*!
+         * \brief check if diameters of 1d fragments do vary significantly within a running window
+         * \param[in] fragments
+         * \param[in] eps
+         * \param[in] running_window
+         * \return \c bool
+         */
+        bool check_diameter_variability
+        (
+            const FRAGMENTS& fragments, 
+            const number eps=0.20, 
+            const int running_window=4
+        );
+
+        /*!
+         * \brief check if branching points are close with respect to their average radii (start and end vertex)
+         * \param[in] fragments
+         * \param[in] eps
+         * \return \c bool
+         */
+        bool check_for_close_branching_points
+        (
+           const FRAGMENTS& fragments, 
+           const number eps=0.20
+        );
+    }
+}
+
+#endif // UG__PLUGINS__NEURO_COLLECTION__TEST__CONSISTENCY_UTIL_H
---
+++ test/consistency_util_impl.cpp
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2009-2021: G-CSC, Goethe University Frankfurt
+ *
+ * Author: Stephan Grein
+ * Creation date: 2021-02-10
+ *
+ * This file is part of NeuroBox, which is based on UG4.
+ *
+ * NeuroBox and UG4 are free software: You can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3
+ * (as published by the Free Software Foundation) with the following additional
+ * attribution requirements (according to LGPL/GPL v3 §7):
+ *
+ * (1) The following notice must be displayed in the appropriate legal notices
+ * of covered and combined works: "Based on UG4 (www.ug4.org/license)".
+ *
+ * (2) The following notice must be displayed at a prominent place in the
+ * terminal output of covered works: "Based on UG4 (www.ug4.org/license)".
+ *
+ * (3) The following bibliography is recommended for citation and must be
+ * preserved in all covered files:
+ * "Reiter, S., Vogel, A., Heppner, I., Rupp, M., and Wittum, G. A massively
+ *   parallel geometric multigrid solver on hierarchically distributed grids.
+ *   Computing and visualization in science 16, 4 (2013), 151-164"
+ * "Vogel, A., Reiter, S., Rupp, M., Nägel, A., and Wittum, G. UG4 -- a novel
+ *   flexible software system for simulating PDE based models on high performance
+ *   computers. Computing and visualization in science 16, 4 (2013), 165-179"
+ * "Stepniewski, M., Breit, M., Hoffer, M. and Queisser, G.
+ *   NeuroBox: computational mathematics in multiscale neuroscience.
+ *   Computing and visualization in science (2019).
+ * "Breit, M. et al. Anatomically detailed and large-scale simulations studying
+ *   synapse loss and synchrony using NeuroBox. Front. Neuroanat. 10 (2016), 8"
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ */
+
+#include "consistency_util.h"
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/mean.hpp>
+#include <boost/accumulators/statistics/variance.hpp>
+#include <boost/range/algorithm/for_each.hpp> 
+#include <boost/bind.hpp>
+#include <boost/ref.hpp>
+
+namespace ug
+{
+    namespace neuro_collection
+    {
+   		////////////////////////////////////////////////////////////////////////
+		/// CHECK_DIAMETER_VARIABILIY
+		////////////////////////////////////////////////////////////////////////
+        bool check_diameter_variability(
+            const FRAGMENTS &fragments, 
+            const number eps, 
+            const int running_window
+        ) 
+        {
+            using namespace boost::accumulators;
+            using namespace boost;
+            const int n = fragments.second.size();
+            for (int i = 0; i < n; i++) {
+                const int m = fragments.second[i].size();
+                for (int j = 0; m / running_window; j++) {
+                    const std::vector<number>& temp = std::vector<number>(fragments.second[i].begin() + j * running_window, fragments.second[i].begin() + (j+1) * running_window - 1);
+                    accumulator_set<number, stats<tag::mean, tag::variance> > acc;
+                    for_each(temp.begin(), temp.end(), bind<void>(ref(acc), _1));
+                    if (variance(acc) > eps) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+
+   		////////////////////////////////////////////////////////////////////////
+		/// CHECK_FOR_CLOSE_BRANCHING_POINTS
+		////////////////////////////////////////////////////////////////////////
+        bool check_for_close_branching_points
+        (
+            const FRAGMENTS &fragments, 
+            const number eps
+        )
+        {
+            const int n = fragments.first.size();
+            for (int i = 0; i < n; i++) {
+                const int m = fragments.second[i].size();
+                number dist = 0;
+                for (int j = 0; j < m-1; j++) {
+                    dist += VecDistance(fragments.first[i][j], fragments.first[i][j+1]);
+                }
+                if (dist < (eps * 0.5 * (fragments.second[i].front() + fragments.second[i].back()))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
